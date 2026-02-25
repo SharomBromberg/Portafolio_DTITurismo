03bb7f0ccf1d5f9d72a68eb298facc33edfcaf26
sharom bromberg <sharombg7@gmail.com>
2026-02-24 22:37:44 -0500

    updated project caché sw

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = "cafe-v1";
+const CACHE_VERSION = "DTI-V1.0";
+const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const ASSETS = [
   "/",
   "/index.html",
@@ -7,10 +8,45 @@ const ASSETS = [
   "/assets/img/mapa_col_00.jpg",
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(STATIC_CACHE).then((cache) => cache.addAll(ASSETS)),
+  );
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key !== STATIC_CACHE)
+            .map((key) => caches.delete(key)),
+        ),
+      )
+      .then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== self.location.origin) {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+        caches
+          .open(STATIC_CACHE)
+          .then((cache) => cache.put(event.request, responseClone));
+        return response;
+      })
+      .catch(() => caches.match(event.request)),
+  );
 });
